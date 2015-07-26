60c8b74b58f7ce91a64b20207eb15919be52871d
sanjiv sahayam <sanjsmailbox@gmail.com>
2015-07-26 18:37:23 +1000

    refactoring find_matching_files

--- scoggle.py
+++ scoggle.py
@@ -20,7 +20,19 @@ class Scoggle:
         return tuple(map(lambda x: (prefix + x) , suffixes))
 
     def prepend_root_dir_to_paths(self, root_dir, src_dirs):
-        return list(map(lambda path: os.path.join(root_dir, path.lstrip(os.path.sep)), src_dirs))    
+        return list(map(lambda path: os.path.join(root_dir, path.lstrip(os.path.sep)), src_dirs))
+
+    # src_dirs - This should be a list absolute paths to the directories to search
+    # suffixes - This should be a tuple of suffixes. suffixes should be of the form ['Name1.ext1', 'Name2.ext2]
+    # its probably better to pass in the file extension as well.
+    def search_dirs__for_files_with_suffix(self, src_dirs, suffixes):
+        matched_files = []
+        for src_dir in fullpath_srcs:
+            for root, dirnames, filenames in os.walk(src_dir):
+                print("filenames: " + str(filenames))
+                hits = [os.path.join(root, f) for f in filenames if f.endswith(suffixes)]
+                matched_files.extend(hits)  
+        return matched_files              
 
     # def find_matching_files(self, root_dir, src_dirs, prefix, suffixes):
     #     matched_files = []
@@ -28,7 +40,7 @@ class Scoggle:
     #     print("suffixes" + str(suffixes))
     #     extensions = self.prepend_prefix_to_suffixes(prefix, suffixes)
     #     print("extensions: " + str(extensions))
-    #     fullpath_srcs = prepend_root_dir_to_paths(root_dir, src_dirs)
+    #     fullpath_srcs = self.prepend_root_dir_to_paths(root_dir, src_dirs)
     #     print("fullpath_srcs: " + str(fullpath_srcs))
 
     #     for src_dir in fullpath_srcs:
