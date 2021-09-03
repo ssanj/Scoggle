82ab1c708719b0154bf00360ace2901e7a2e36c0
sanjiv sahayam <sanjsmailbox@gmail.com>
2021-09-03 17:00:51 +1000

    Add more tests

--- tests/test_file_path_creator_test.py
+++ tests/test_file_path_creator_test.py
@@ -5,7 +5,7 @@ import os
 
 class TestFilePathCreatorTest(unittest.TestCase):
 
-  def test_get_test_file_class_name(self):
+  def test_get_test_file_params(self):
       root_dir = "/some/root/dir"
       package_dir = "my/awesome/project/"
       test_srcs = ["/src/test/"]
@@ -16,5 +16,41 @@ class TestFilePathCreatorTest(unittest.TestCase):
       creator = tftypes.TestFilePathCreator(params, None)
 
       created_path = creator.get_test_file_path(0)
+      test_file_class_name = creator.get_test_file_class_name()
+      package_path = creator.get_dotted_package_path()
 
       self.assertEqual(created_path, "/some/root/dir/src/test/my/awesome/project/AwesomeThingSuite.scala")
+      self.assertEqual(test_file_class_name, "AwesomeThingSuite")
+      self.assertEqual(package_path, "my.awesome.project")
+
+  def test_get_test_file_params_with_new_file(self):
+      root_dir = "/some/root/dir"
+      package_dir = "my/awesome/project/"
+      test_srcs = ["/src/test/"]
+      file_name = "AwesomeThing"
+      suffix = "Suite.scala"
+      params = stypes.TestFileCreationParam(root_dir, package_dir, test_srcs, file_name, suffix)
+
+      creator = tftypes.TestFilePathCreator(params, None)
+
+      # This changes the file name (nothing else)
+      new_creator = creator.with_new_test_file_name("AnotherThingThatIsCoolSpec.scala")
+
+      # Old Creator
+      created_path = creator.get_test_file_path(0)
+      test_file_class_name = creator.get_test_file_class_name()
+      package_path = creator.get_dotted_package_path()
+
+      self.assertEqual(created_path, "/some/root/dir/src/test/my/awesome/project/AwesomeThingSuite.scala")
+      self.assertEqual(test_file_class_name, "AwesomeThingSuite")
+      self.assertEqual(package_path, "my.awesome.project")
+
+      # New creator
+      new_created_path = new_creator.get_test_file_path(0)
+      new_test_file_class_name = new_creator.get_test_file_class_name()
+      new_package_path = new_creator.get_dotted_package_path()
+
+      self.assertEqual(new_created_path, "/some/root/dir/src/test/my/awesome/project/AnotherThingThatIsCoolSpec.scala")
+      self.assertEqual(new_test_file_class_name, "AnotherThingThatIsCoolSpec")
+      self.assertEqual(new_package_path, "my.awesome.project")
+
